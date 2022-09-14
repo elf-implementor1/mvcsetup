eaedfb787cc3a03511b41f878dfca923ad00e94e
jetpackz12 <jetpackz12>
2022-09-14 10:12:35 +0800

    first commit

---
+++ app/lib/constant.php
@@ -0,0 +1,10 @@
+<?php
+//define("ROOT", "/");
+define("TITLE", "MVCSETUP");
+define("ROOT", "/MVCSETUP/");
+define("BOOTSTRAP", "public/");
+define("PATH_CONTROLLER", "app/controllers/");
+define("APPROOT", "app");
+define("PATH_VIEW", "app/views/");
+define("PATH_MODEL", "app/models/");
+?>
